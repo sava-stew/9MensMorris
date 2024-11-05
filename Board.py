50839148ad981d2a54bb820d99605fafc7ea02a7
dsponsler <dpsfgp@umsystem.edu>
2024-11-04 20:12:20 -0600

    allow for moving a piece

--- Board.py
+++ Board.py
@@ -24,30 +24,30 @@ from Player import Player
 #function should access the key (button name) and change index[0] of list to player
 
 placements = {
-    'g1': ['open', 'noMill', 20, 20], #g1 g4 and g7 are all the same color, you should change noMill to Mill for g1 g4 and g7
-    'g4': ['open', 'noMill', 245, 20],
-    'g7': ['open', 'noMill', 480, 20],
-    'f2': ['open', 'noMill', 105, 90],
-    'f4': ['open', 'noMill', 245, 90],
-    'f6': ['open', 'noMill', 385, 90],
-    'e3': ['open', 'noMill', 175, 160],
-    'e4': ['open', 'noMill', 245, 160],
-    'e5': ['open', 'noMill', 315, 160],
-    'd1': ['open', 'noMill', 20, 245],
-    'd2': ['open', 'noMill', 105, 245],
-    'd3': ['open', 'noMill', 175, 245],
-    'd5': ['open', 'noMill', 315, 245],
-    'd6': ['open', 'noMill', 385, 245],
-    'd7': ['open', 'noMill', 480, 245],
-    'c3': ['open', 'noMill', 175, 340],
-    'c4': ['open', 'noMill', 245, 340],
-    'c5': ['open', 'noMill', 315, 340],
-    'b2': ['open', 'noMill', 105, 410],
-    'b4': ['open', 'noMill', 245, 410],
-    'b6': ['open', 'noMill', 385, 410],
-    'a1': ['open', 'noMill', 20, 480],
-    'a4': ['open', 'noMill', 245, 480],
-    'a7': ['open', 'noMill', 480, 480],
+    'g1': ['open', 'noMill', 20, 20, "g1", ["g4","d1"]], #g1 g4 and g7 are all the same color, you should change noMill to Mill for g1 g4 and g7
+    'g4': ['open', 'noMill', 245, 20, "g4", ["g1","g7","f4"]],
+    'g7': ['open', 'noMill', 480, 20, "g7", ["g4","d4"]],
+    'f2': ['open', 'noMill', 105, 90, "f2", ["f4","d2"]],
+    'f4': ['open', 'noMill', 245, 90, "f4", ["f2","g4","f6","e4"]],
+    'f6': ['open', 'noMill', 385, 90, "f6", ["f4","d6"]],
+    'e3': ['open', 'noMill', 175, 160, "e3", ["e4","d3"]],
+    'e4': ['open', 'noMill', 245, 160, "e4", ["e3","f4","e5"]],
+    'e5': ['open', 'noMill', 315, 160, "e5", ["e4","d5"]],
+    'd1': ['open', 'noMill', 20, 245, "d1", ["g1","d2","a1"]],
+    'd2': ['open', 'noMill', 105, 245, "d2", ["d1","f2","d3","b2"]],
+    'd3': ['open', 'noMill', 175, 245, "d3", ["d2","e3","c3"]],
+    'd5': ['open', 'noMill', 315, 245, "d5", ["e5","d6","c5"]],
+    'd6': ['open', 'noMill', 385, 245, "d6", ["d5","f6","d7","b6"]],
+    'd7': ['open', 'noMill', 480, 245, "d7", ["d6","g7","a7"]],
+    'c3': ['open', 'noMill', 175, 340, "c3", ["d3","c4"]],
+    'c4': ['open', 'noMill', 245, 340, "c4", ["c3","c5","b4"]],
+    'c5': ['open', 'noMill', 315, 340, "c5", ["c4","d5"]],
+    'b2': ['open', 'noMill', 105, 410, "b2", ["d2","b4"]],
+    'b4': ['open', 'noMill', 245, 410, "b4", ["b2","c4","a4","b6"]],
+    'b6': ['open', 'noMill', 385, 410, "b6", ["b4","d6"]],
+    'a1': ['open', 'noMill', 20, 480, "a1", ["a4","d1"]],
+    'a4': ['open', 'noMill', 245, 480, "a4", ["a1","b4","a7"]],
+    'a7': ['open', 'noMill', 480, 480, "a7", ["a4","d7"]]
 }
 
 black = Player()
@@ -151,15 +151,15 @@ class Board(tk.Tk):
     def drawButtons(self, canvas, turn):
         for placement in placements.values():
             #print(type(placement[4]))
-            placement[4].config(command=lambda i=placement: Board.onButtonPress(self, i, canvas, turn))
+            placement[6].config(command=lambda i=placement: Board.onButtonPress(self, i, canvas, turn))
             if placement[0] == "open":
-                canvas.create_window(placement[2], placement[3], window=placement[4])
+                canvas.create_window(placement[2], placement[3], window=placement[6])
             elif placement[0] == "white":
-                placement[4].config(text="", height=3, width=5, bg="white")
-                canvas.create_window(placement[2], placement[3], window=placement[4])
+                placement[6].config(text="", height=3, width=5, bg="white")
+                canvas.create_window(placement[2], placement[3], window=placement[6])
             elif placement[0] == "black":
-                placement[4].config(text="", height=3, width=5, bg="black")
-                canvas.create_window(placement[2], placement[3], window=placement[4])
+                placement[6].config(text="", height=3, width=5, bg="black")
+                canvas.create_window(placement[2], placement[3], window=placement[6])
         return
 
     def setUp(self):
@@ -169,16 +169,44 @@ class Board(tk.Tk):
 
     def onButtonPress(self, button, canvas, turn):
         print(button)
-        if button[0] == "open" and turn.getTurn() == 'black' and black.getBankPieces() != 0:
-            #button.config(text="",height=3,width=5,bg="black")
-            black.bankUpdate()
-            button[0] = "black"
-            turn.changeTurn()
-        elif button[0] == "open" and turn.getTurn() == 'white' and white.getBankPieces() != 0:
-            #button.config(text="",height=3,width=5,bg="white")
-            white.bankUpdate()
-            button[0] = "white"
-            turn.changeTurn()
+        whoseTurn = turn.getTurn()
+        if whoseTurn == "white":
+            bankPieces = white.getBankPieces()
+            boardPieces = white.getPlayerPieces()
+        else:
+            bankPieces = black.getBankPieces()
+            boardPieces = black.getPlayerPieces()
+
+        #if the player still has pieces left in the bank, go in "place mode"
+        if bankPieces != 0:
+            if button[0] == "open" and whoseTurn == 'black' and black.getBankPieces() != 0:
+                #button.config(text="",height=3,width=5,bg="black")
+                black.bankUpdate()
+                button[0] = "black"
+                turn.changeTurn()
+            elif button[0] == "open" and whoseTurn == 'white' and white.getBankPieces() != 0:
+                #button.config(text="",height=3,width=5,bg="white")
+                white.bankUpdate()
+                button[0] = "white"
+                turn.changeTurn()
+
+        #if the player has more than 3 pieces left on the board, go in "move mode"
+        elif boardPieces > 3:
+            #check turn
+            #if color matches turn, go into "wait for another button press mode"
+            #if pressed button = valid move, move piece
+            to_pos = simpledialog.askstring("Move a Piece",
+                                                     f"Select a space to move to:\n" + "\n".join(
+                                                         button[5]))
+            self.move_piece(button[4],to_pos)
+
+        #if the player has only 3 pieces left, go in "fly mode"
+        elif boardPieces <= 3:
+            to_pos = simpledialog.askstring("Move a Piece",
+                                            f"Select a space to move to:\n" + "\n".join(
+                                                placements))
+            self.move_piece(button[4], to_pos)
+
         self.banks()
         self.checkMills()
         self.drawButtons(canvas, turn)
@@ -228,10 +256,10 @@ class Board(tk.Tk):
         if placements[from_pos][0] != 'open' and placements[to_pos][0] == 'open':
 
             placements[to_pos][0] = placements[from_pos][0]
-            placements[to_pos][4].config(text="", height=3, width=5, bg=placements[to_pos][0])
+            placements[to_pos][6].config(text="", height=3, width=5, bg=placements[to_pos][0])
 
             placements[from_pos][0] = 'open'
-            placements[from_pos][4].config(text="O", height=1, width=3, bg="SystemButtonFace")
+            placements[from_pos][6].config(text="O", height=1, width=3, bg="SystemButtonFace")
 
             return True
         else:
@@ -251,7 +279,7 @@ class Board(tk.Tk):
 
         if piece_to_remove in opponent_pieces:
             placements[piece_to_remove][0] = 'open'
-            placements[piece_to_remove][4].config(text="O", height=1, width=3, bg="SystemButtonFace")
+            placements[piece_to_remove][6].config(text="O", height=1, width=3, bg="SystemButtonFace")
             print(f"Removed {opponent_color} piece at {piece_to_remove}.")
         else:
             print("Invalid piece selected.")
